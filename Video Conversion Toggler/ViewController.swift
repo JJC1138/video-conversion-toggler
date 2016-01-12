6dc53ec209f07e5a4eb9700bd4626b42b28e26a2
Jon Colverson <github@vcxz.co.uk>
2016-01-12 11:35:29 +0000

    Added new OS X Cocoa application build target (untouched from template so far).

---
+++ Video Conversion Toggler/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Video Conversion Toggler
+//
+//  Created by Jon Colverson on 12/01/2016.
+//
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
