6b7669b87db9d73f75289c9396d248dc014b6970
Jon Colverson <github@vcxz.co.uk>
2016-01-12 17:42:46 +0000

    Added some fairly ridiculous code to run the run loop while our operations are running because that makes the dispatch queues that Alamofire uses work.

--- OS X/CLI.swift
+++ OS X/CLI.swift
@@ -1,11 +1,32 @@
 import Foundation
 
+func runRunLoopUntilAllOperationsAreFinished(onQueue queue: NSOperationQueue) {
+    let runLoopStopperQueue = NSOperationQueue()
+    class RunLoopStopper: NSOperation {
+        let queue: NSOperationQueue
+        let runLoop: CFRunLoop
+        init(queue: NSOperationQueue, runLoop: CFRunLoop) {
+            self.queue = queue
+            self.runLoop = runLoop
+        }
+        override func main() {
+            while !CFRunLoopIsWaiting(runLoop) {
+                // We need to make sure that the run loop has started. Otherwise if queue finishes quickly we could stop the run loop before we've started it, and then when it is started nothing would stop it. We really just want to know if the run loop is running, but there's no API for that. CFRunLoopIsWaiting(_) returns false if the run loop isn't running, though, so it will do.
+            }
+            queue.waitUntilAllOperationsAreFinished()
+            CFRunLoopStop(runLoop)
+        }
+    }
+    runLoopStopperQueue.addOperation(RunLoopStopper(queue: queue, runLoop: CFRunLoopGetCurrent()))
+    CFRunLoopRun()
+}
+
 func cli() {
     let operationQueue = NSOperationQueue()
     
     operationQueue.addOperation(ToggleSettingOperation(deviceInfo: DeviceInfo(hostname: Process.arguments[2])))
     
-    operationQueue.waitUntilAllOperationsAreFinished()
+    runRunLoopUntilAllOperationsAreFinished(onQueue: operationQueue)
     
     for deviceInfo in deviceSettings.keys() {
         guard let setting = deviceSettings[deviceInfo] else { continue }
