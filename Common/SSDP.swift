68fb9d7b19171fb699e67d40d4f7cc8cdda9f565
Jon Colverson <github@vcxz.co.uk>
2016-01-14 20:46:01 +0000

    Added interpretation of the SSDP response packets.

--- Common/SSDP.swift
+++ Common/SSDP.swift
@@ -7,31 +7,40 @@ import CocoaAsyncSocket
 // and licensed under the Apache License, Version 2.0:
 // http://www.apache.org/licenses/LICENSE-2.0
 
-func discoverSSDPDevices(serviceType serviceType: String, runForSeconds: Int) {
+public func discoverSSDPDevices(serviceType serviceType: String) {
+    let maximumResponseWaitingTimeSeconds = 1
     let ip = "239.255.255.250"
     let port: UInt16 = 1900
-    let message = [
+    let searchMessage = [
         "M-SEARCH * HTTP/1.1",
         "HOST: \(ip):\(port)",
         "MAN: \"ssdp:discover\"",
         "ST: \(serviceType)",
-        "MX: 3",
+        "MX: \(maximumResponseWaitingTimeSeconds)",
         "",
         "",
         ].joinWithSeparator("\r\n").dataUsingEncoding(NSUTF8StringEncoding)
     
     class Delegate: GCDAsyncUdpSocketDelegate {
         @objc func udpSocket(sock: GCDAsyncUdpSocket!, didReceiveData data: NSData!, fromAddress address: NSData!, withFilterContext filterContext: AnyObject!) {
-            print("received packet") // FIXME remove
-            // FIXME implement
+            let responseMessage = CFHTTPMessageCreateEmpty(nil, false).takeRetainedValue()
+            guard CFHTTPMessageAppendBytes(responseMessage, UnsafePointer(data.bytes), data.length) else { return }
+            guard CFHTTPMessageIsHeaderComplete(responseMessage) else { return }
+            guard let originalHeaders = CFHTTPMessageCopyAllHeaderFields(responseMessage)?.takeRetainedValue() as Dictionary? else { return }
+            var headers = [String: String]()
+            for (k, v) in originalHeaders { headers[(k as! String).uppercaseString] = (v as! String) }
+            guard headers["LOCATION"] != nil && headers["ST"] != nil && headers["USN"] != nil else { return }
+            
+            // FIXME Do something useful with headers. Maybe populate a struct with the required parts and an 'extraHeaders' dictionary.
+            print(headers["LOCATION"]) // FIXME remove
         }
     }
     
-    let delegate = Delegate()
+    let delegate = Delegate() // We have to keep a reference to this so it can't be inlined in the call below.
     let sock = GCDAsyncUdpSocket(delegate: delegate, delegateQueue: dispatch_queue_create(nil, DISPATCH_QUEUE_CONCURRENT))
     
-    sock.sendData(message, toHost: ip, port: port, withTimeout: -1, tag: 0)
+    sock.sendData(searchMessage, toHost: ip, port: port, withTimeout: -1, tag: 0)
     try! sock.beginReceiving()
     
-    sleep(UInt32(runForSeconds))
+    sleep(UInt32(maximumResponseWaitingTimeSeconds * 2))
 }
