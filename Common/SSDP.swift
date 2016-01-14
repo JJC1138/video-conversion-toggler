5fbb3404554a897a074fde3f14a255fecc7ffcba
Jon Colverson <github@vcxz.co.uk>
2016-01-14 18:46:50 +0000

    Tidied DiscoverSSDPDevices again.

--- Common/SSDP.swift
+++ Common/SSDP.swift
@@ -18,15 +18,6 @@ class DiscoverSSDPDevices: NSOperation {
     }
     
     override func main() {
-        do {
-            try run()
-        } catch let e {
-            print(e) // FIXME remove
-            // FIXME handle errors; maybe just log since this shouldn't be happening unless something unrecoverable is happening
-        }
-    }
-    
-    func run() throws {
         let ip = "239.255.255.250"
         let port: UInt16 = 1900
         let message = [
@@ -40,19 +31,17 @@ class DiscoverSSDPDevices: NSOperation {
             ].joinWithSeparator("\r\n").dataUsingEncoding(NSUTF8StringEncoding)
         
         class Delegate: GCDAsyncUdpSocketDelegate {
-            
             @objc func udpSocket(sock: GCDAsyncUdpSocket!, didReceiveData data: NSData!, fromAddress address: NSData!, withFilterContext filterContext: AnyObject!) {
                 print("received packet") // FIXME remove
                 // FIXME implement
             }
-            
         }
         
         let delegate = Delegate()
         let sock = GCDAsyncUdpSocket(delegate: delegate, delegateQueue: dispatch_queue_create(nil, DISPATCH_QUEUE_CONCURRENT))
         
         sock.sendData(message, toHost: ip, port: port, withTimeout: -1, tag: 0)
-        try sock.beginReceiving()
+        try! sock.beginReceiving()
         
         sleep(UInt32(runForSeconds))
     }
