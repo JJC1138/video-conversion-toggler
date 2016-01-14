92ab4fbcaf820abf3437bb58ab7d38073c8ed23a
Jon Colverson <github@vcxz.co.uk>
2016-01-14 18:54:55 +0000

    Changed discoverSSDPDevices into a plain function.

--- Common/SSDP.swift
+++ Common/SSDP.swift
@@ -7,34 +7,31 @@ import CocoaAsyncSocket
 // and licensed under the Apache License, Version 2.0:
 // http://www.apache.org/licenses/LICENSE-2.0
 
-func discoverSSDPDevices(serviceType serviceType: String, runForSeconds: Int) -> () -> Void {
-    return {
-        let ip = "239.255.255.250"
-        let port: UInt16 = 1900
-        let message = [
-            "M-SEARCH * HTTP/1.1",
-            "HOST: \(ip):\(port)",
-            "MAN: \"ssdp:discover\"",
-            "ST: \(serviceType)",
-            "MX: 3",
-            "",
-            "",
-            ].joinWithSeparator("\r\n").dataUsingEncoding(NSUTF8StringEncoding)
-        
-        class Delegate: GCDAsyncUdpSocketDelegate {
-            @objc func udpSocket(sock: GCDAsyncUdpSocket!, didReceiveData data: NSData!, fromAddress address: NSData!, withFilterContext filterContext: AnyObject!) {
-                print("received packet") // FIXME remove
-                // FIXME implement
-            }
+func discoverSSDPDevices(serviceType serviceType: String, runForSeconds: Int) {
+    let ip = "239.255.255.250"
+    let port: UInt16 = 1900
+    let message = [
+        "M-SEARCH * HTTP/1.1",
+        "HOST: \(ip):\(port)",
+        "MAN: \"ssdp:discover\"",
+        "ST: \(serviceType)",
+        "MX: 3",
+        "",
+        "",
+        ].joinWithSeparator("\r\n").dataUsingEncoding(NSUTF8StringEncoding)
+    
+    class Delegate: GCDAsyncUdpSocketDelegate {
+        @objc func udpSocket(sock: GCDAsyncUdpSocket!, didReceiveData data: NSData!, fromAddress address: NSData!, withFilterContext filterContext: AnyObject!) {
+            print("received packet") // FIXME remove
+            // FIXME implement
         }
-        
-        let delegate = Delegate()
-        let sock = GCDAsyncUdpSocket(delegate: delegate, delegateQueue: dispatch_queue_create(nil, DISPATCH_QUEUE_CONCURRENT))
-        
-        sock.sendData(message, toHost: ip, port: port, withTimeout: -1, tag: 0)
-        try! sock.beginReceiving()
-        
-        sleep(UInt32(runForSeconds))
     }
     
+    let delegate = Delegate()
+    let sock = GCDAsyncUdpSocket(delegate: delegate, delegateQueue: dispatch_queue_create(nil, DISPATCH_QUEUE_CONCURRENT))
+    
+    sock.sendData(message, toHost: ip, port: port, withTimeout: -1, tag: 0)
+    try! sock.beginReceiving()
+    
+    sleep(UInt32(runForSeconds))
 }
