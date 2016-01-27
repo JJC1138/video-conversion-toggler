7a6fadf1236529ed1eadc7f888927798b44a5822
Jon Colverson <github@vcxz.co.uk>
2016-01-27 17:42:14 +0000

    Added an awakeUptime() utility function.

--- Common/Utility.swift
+++ Common/Utility.swift
@@ -16,3 +16,14 @@ func localString(format format: String, _ arguments: CVarArgType...) -> String {
     // It's important to use NSLocale.currentLocale() because that gives the user's locale, whereas by default this initializer uses the system's locale.
     return String(format: format, locale: NSLocale.currentLocale(), arguments: arguments)
 }
+
+// Based on https://developer.apple.com/library/mac/qa/qa1398/_index.html
+private let machTimebaseMultiplier: NSTimeInterval = {
+    var info = mach_timebase_info()
+    mach_timebase_info(&info)
+    return NSTimeInterval(info.numer) / NSTimeInterval(info.denom)
+}()
+
+func awakeUptime() -> NSTimeInterval {
+    return NSTimeInterval(mach_absolute_time()) * machTimebaseMultiplier
+}
