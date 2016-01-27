59ce1f181de9f96d38cebbeb4ac57d211f4f2247
Jon Colverson <github@vcxz.co.uk>
2016-01-27 14:04:27 +0000

    Moved handling of app becoming/resigning active status into the view controller.

--- tvOS/AppDelegate.swift
+++ tvOS/AppDelegate.swift
@@ -2,17 +2,5 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
     var window: UIWindow?
-    
-    let oq = NSOperationQueue()
-    
-    func applicationDidBecomeActive(application: UIApplication) {
-        oq.addOperation(PeriodicallyFetchAllStatuses())
-    }
-    
-    func applicationWillResignActive(application: UIApplication) {
-        oq.cancelAllOperations()
-    }
-    
 }
--- tvOS/ViewController.swift
+++ tvOS/ViewController.swift
@@ -1,4 +1,53 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let oq = NSOperationQueue()
+    
+    class PeriodicallyFetchAllStatuses: NSOperation {
+        
+        override func main() {
+            let fetchQueue = NSOperationQueue()
+            while (!cancelled) {
+                discoverCompatibleDevices { deviceInfo in fetchQueue.addOperation(NSBlockOperation() {
+                    do {
+                        let setting = try fetchSetting(deviceInfo)
+                        // FIXME handle results
+                        print("\(deviceInfo): \(setting)") // FIXME remove
+                    } catch let e as AppError {
+                        // FIXME handle errors
+                    } catch {}
+                    })
+                }
+            }
+            fetchQueue.waitUntilAllOperationsAreFinished()
+        }
+        
+    }
+    
+    override func viewDidLoad() {
+        let nc = NSNotificationCenter.defaultCenter()
+        nc.addObserverForName(UIApplicationDidBecomeActiveNotification, object: nil, queue: nil, usingBlock: applicationDidBecomeActive)
+        nc.addObserverForName(UIApplicationWillResignActiveNotification, object: nil, queue: nil, usingBlock: applicationWillResignActive)
+    }
+    
+    func applicationDidBecomeActive(_: NSNotification) {
+        oq.addOperation(PeriodicallyFetchAllStatuses())
+    }
+    
+    func applicationWillResignActive(_: NSNotification) {
+        oq.cancelAllOperations()
+    }
+    
+//    // MARK: UITableViewDataSource
+//    
+//    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        assert(section == 0)
+//        // FIXME implement
+//    }
+//    
+//    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//        // FIXME implement
+//    }
+    
 }
