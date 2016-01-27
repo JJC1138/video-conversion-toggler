fbf75371914353f6a1284aaa35cba090eb03317d
Jon Colverson <github@vcxz.co.uk>
2016-01-27 15:31:18 +0000

    Hooked up table view to the view controller and started populating its cells.

--- tvOS/ViewController.swift
+++ tvOS/ViewController.swift
@@ -1,12 +1,22 @@
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
     
+    @IBOutlet weak var deviceTable: UITableView!
     let oq = NSOperationQueue()
     
+    struct DeviceSetting {
+        let device: DeviceInfo
+        let setting: Bool
+    }
+    
+    var deviceSettings = [DeviceSetting]()
+    
     func newFetchResult(deviceInfo: DeviceInfo, setting: Bool) {
-        // FIXME implement
-        print("\(deviceInfo): \(setting)") // FIXME remove
+        // FIXME replace existing entry for this device if present
+        deviceSettings.append(DeviceSetting(device: deviceInfo, setting: setting))
+        // FIXME handle this more precisely:
+        deviceTable.reloadData()
     }
     
     func newFetchError(deviceInfo: DeviceInfo, error: AppError) {
@@ -14,6 +24,8 @@ class ViewController: UIViewController {
     }
     
     override func viewDidLoad() {
+        deviceTable.dataSource = self
+        
         let nc = NSNotificationCenter.defaultCenter()
         nc.addObserverForName(UIApplicationDidBecomeActiveNotification, object: nil, queue: nil, usingBlock: applicationDidBecomeActive)
         nc.addObserverForName(UIApplicationWillResignActiveNotification, object: nil, queue: nil, usingBlock: applicationWillResignActive)
@@ -27,15 +39,21 @@ class ViewController: UIViewController {
         oq.cancelAllOperations()
     }
     
-//    // MARK: UITableViewDataSource
-//    
-//    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        assert(section == 0)
-//        // FIXME implement
-//    }
-//    
-//    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-//        // FIXME implement
-//    }
+    // MARK: UITableViewDataSource
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        assert(section == 0)
+        return deviceSettings.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        assert(indexPath.section == 0)
+        let deviceSetting = deviceSettings[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("Device", forIndexPath: indexPath)
+        cell.textLabel!.text = deviceSetting.device.description
+        cell.detailTextLabel!.text = localString(deviceSetting.setting ? "On" : "Off")
+        return cell
+    }
     
 }
