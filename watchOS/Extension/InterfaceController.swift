c08b03ac01acdf1ddb92eb22125dcf080f10bece
Jon Colverson <github@vcxz.co.uk>
2016-02-04 17:12:00 +0000

    Removed watchOS template cruft.

--- watchOS/Extension/InterfaceController.swift
+++ watchOS/Extension/InterfaceController.swift
@@ -1,31 +1,4 @@
-//
-//  InterfaceController.swift
-//  Video Conversion Toggler Extension
-//
-//  Created by Jon Colverson on 2016-02-04.
-//
-//
-
 import WatchKit
-import Foundation
-
 
 class InterfaceController: WKInterfaceController {
-
-    override func awakeWithContext(context: AnyObject?) {
-        super.awakeWithContext(context)
-        
-        // Configure interface objects here.
-    }
-
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-        super.willActivate()
-    }
-
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
-    }
-
 }
