0763a2c0b6355dc9e37ccf74cdfe9e3d7e330447
Jon Colverson <github@vcxz.co.uk>
2016-02-11 22:22:34 +0000

    OS X: Made app terminate when its window is closed.

--- OS X/AppDelegate.swift
+++ OS X/AppDelegate.swift
@@ -1,4 +1,9 @@
 import Cocoa
 
 class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationShouldTerminateAfterLastWindowClosed(sender: NSApplication) -> Bool {
+        return true
+    }
+    
 }
